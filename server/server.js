57a5a3fa3e03d08f9548cdd7ca84dfeba890d786
tyler broadway <tbroadway96@gmail.com>
2020-06-04 22:54:08 -0500

    Create user is functional

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
-const router = require('./usersRouter');
+const bcrypt = require('bcrypt');
+const Users = require('./models');
 
 const server = express();
 server.use(express.json());
 
-server.use('/api/register', router);
-server.use('/api/login', router);
-server.use('/api/users', router);
+server.post('/api/register', async (req, res) => {
+    let body = req.body;
+
+    if (!body.username || !body.password) {
+        res.status(500).json({ message: 'You must provide a username & password.' });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hashSync(body.password, salt);
+    body.password = hash;
+
+    const newUser = await Users.createUser(body);
+
+    if (newUser) {
+        res.status(200).json(newUser);
+    } else {
+        res.status(500).json({ 
+            message: 'There was a problem with creating your account'
+        });
+    }
+})
 
 module.exports = server;
